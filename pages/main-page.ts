38993ef0f2ef1b6a62076222a91a8d4cb16d6536
Taras Karsakov <taras.karsakov@intexsoft.by>
2024-06-04 13:12:49 +0300

    Add MainPage POM and test for it

--- pages/main-page.ts
+++ pages/main-page.ts
@@ -2,4 +2,23 @@ import { type Page, type Locator } from "@playwright/test";
 
 export class MainPage {
   readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  getConstructorSectionHeader(text: string) {
+    return this.page.locator(`xpath=//h2[text()="${text}"]`);
+  }
+
+  async goto() {
+    await this.page.goto("/");
+  }
+
+  async clickConstructorSectionByText(text: string) {
+    await this.page
+      .locator(`xpath=//span[text()="${text}"]/parent::div`)
+      .click();
+    await this.page.waitForTimeout(1500);
+  }
 }
